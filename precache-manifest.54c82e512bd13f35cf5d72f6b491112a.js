a7eec18387b02002672feb7dfc1a7b1b8335e175
RachelHB <105057555+RachelHB@users.noreply.github.com>
2022-07-01 10:03:13 +0100

    Updates

---
+++ precache-manifest.54c82e512bd13f35cf5d72f6b491112a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7ecf656820b0e72c04f6e9d5e7d1ce",
+    "url": "/Project-9---Front-End-Event-App/index.html"
+  },
+  {
+    "revision": "aa2a9164260f2a35d892",
+    "url": "/Project-9---Front-End-Event-App/static/css/main.dccd1fea.chunk.css"
+  },
+  {
+    "revision": "ca9dd0c0acbc93503294",
+    "url": "/Project-9---Front-End-Event-App/static/js/2.fcb46e98.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/Project-9---Front-End-Event-App/static/js/2.fcb46e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2a9164260f2a35d892",
+    "url": "/Project-9---Front-End-Event-App/static/js/main.c4e4241e.chunk.js"
+  },
+  {
+    "revision": "ddc9c40206aaa00bbff2",
+    "url": "/Project-9---Front-End-Event-App/static/js/runtime-main.0a9a7bfc.js"
+  },
+  {
+    "revision": "33ef4db5db435395636402565a3018ba",
+    "url": "/Project-9---Front-End-Event-App/static/media/event image 1.33ef4db5.jpg"
+  }
+]);
